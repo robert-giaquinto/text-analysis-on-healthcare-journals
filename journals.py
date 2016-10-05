767de8a9a61da91d80ec38279438f746e2eb9d31
robert-giaquinto <giaquinto.ra@gmail.com>
2016-10-05 11:25:31 -0500

    Added Shaya and my text cleaning steps. Haven't tested this because MSI is down.

--- journals.py
+++ journals.py
@@ -2,6 +2,11 @@ from __future__ import division, print_function
 import os
 import logging
 import argparse
+from nltk.corpus import stopwords, wordnet
+from nltk.stem import WordNetLemmatizer
+from nltk import pos_tag
+import re
+
 from parse_journal.collect_journal_keys import KeyCollector
 
 logger = logging.getLogger(__name__)
@@ -40,6 +45,41 @@ class Journals(object):
             self.stream = self.init_generator()
         else:
             self.stream = None
+
+        # store variables for cleaning text here, so it's accessible to all worker nodes
+        self.lemmatizer = WordNetLemmatizer()        
+        self.stopword_set = set(stopwords.words("english"))
+        self.split_dash = re.compile(r"([a-zA-Z])([-/])([a-zA-Z])")
+        self.punct = re.compile(r"[^a-zA-Z_ ]") # keeping _'s in so we can use them as a special identifier
+
+        self.iam = re.compile(r"'m")
+        self.willnot = re.compile(r"wont't")
+        self.nt = re.compile(r"n't")
+        self.itis = re.compile(r"it's")
+        self.letus = re.compile(r"let's")
+        self.heis = re.compile(r"he's")
+        self.howis = re.compile(r"how's")
+        self.thatis = re.compile(r"that's")
+        self.thereis = re.compile(r"there's")
+        self.whatis = re.compile(r"what's")
+        self.whereis = re.compile(r"where's")
+        self.whenis = re.compile(r"when's")
+        self.whois = re.compile(r"who's")
+        self.whyis = re.compile(r"why's")
+        self.youall = re.compile(r"y'all")
+        self.would = re.compile(r"'d")
+        self.has = re.compile(r"'s")
+        self.youare = re.compile(r"you're")
+        self.will = re.compile("'ll")
+        self.s_apostrophe = re.compile("s' ")
+
+        # here are some other regexs to capture certain patters, but it might be more efficient
+        # to run these on the whole text final (via grep from command line)
+        self.html = re.compile(r"<[^>]*>")
+        self.years = re.compile(r"\b(18|19|20)[0-9]{2}\b")
+        self.punct = re.compile(r"[^a-zA-Z_\s]")
+        #self.dollars = re.compile(r"\$[1-9][0-9,\.]+")
+        # email addresses? times? dates? numbers? 
 
     def init_keys_file(self, keys_file):
         # check is a keys file is given (and if it really exists), if not create one
@@ -84,6 +124,25 @@ class Journals(object):
                 journal = Journal(siteId=siteId, userId=userId, journalId=journalId, createdAt=createdAt, body=body)
                 yield journal
 
+    def get_wordnet_pos(self, treebank_tag):
+        """
+        Need to know the part of speech of each word to properly lemmatize.
+        this function standardizes the POS codes so that they're understandable
+        by the lemmatizing function.
+        :param treebank_tag:
+        :return:
+        """
+        if treebank_tag.startswith('J'):
+            return wordnet.ADJ
+        elif treebank_tag.startswith('V'):
+            return wordnet.VERB
+        elif treebank_tag.startswith('N'):
+            return wordnet.NOUN
+        elif treebank_tag.startswith('R'):
+            return wordnet.ADV
+        else:
+            return ''
+
     def clean_journal(self, journal):
         """
         Implement function for how to clean text of a journal
@@ -94,8 +153,53 @@ class Journals(object):
                  so that the text is represented as a list of words
         
         """
+        # split hyphens and slashes
+        journal.body = self.split_dash.sub("$1 $3", journal.body)
+        
+        # homogenize special patterns
+        journal.body = self.years.sub(" _year_ ", journal.body)
+
+        # save emojis
+        # ??? don't know how, what emojis are out there?
+
+        # expand contractions
+        journal.body = self.iam.sub(" am", journal.body)
+        journal.body = self.willnot.sub("will not", journal.body)
+        journal.body = self.nt.sub(" not", journal.body)
+        journal.body = self.itis.sub("it is", journal.body)
+        journal.body = self.letus.sub("let us", journal.body)
+        journal.body = self.heis.sub("he is", journal.body)
+        journal.body = self.howis.sub("how is", journal.body)
+        journal.body = self.thatis.sub("that is", journal.body)
+        journal.body = self.thereis.sub("there is", journal.body)
+        journal.body = self.whatis.sub("what is", journal.body)
+        journal.body = self.whereis.sub("where is", journal.body)
+        journal.body = self.whenis.sub("when is", journal.body)
+        journal.body = self.whois.sub("who is", journal.body)
+        journal.body = self.whyis.sub("why is", journal.body)
+        journal.body = self.youall.sub("you all", journal.body)
+        journal.body = self.would.sub(" would", journal.body)
+        journal.body = self.youare.sub("you are", journal.body)
+        journal.body = self.will.sub(" will", journal.body)
+        journal.body = self.s_apostrophe.sub(" has", journal.body)
+        journal.body = self.has.sub(" has", journal.body)
+
+        # remove html
+        journal.body = self.html.sub(" ", journal.body)
+
+        # remove remaining punctuation (except underscores)
+        journal.body = self.punct.sub(" ", journal.body)
+
+        # to lowercase and tokenize
         journal.body = journal.body.lower().split()
-        journal.tokenized = True
+        journal.tokenized = True # setting this flag helps printing journal objects
+
+        # remove stopwords
+        journal.body = [w for w in journal.body in w not in self.stopword_set]
+        # lemmatize the remaining tokens
+        journal.body = [self.lemmatizer.lemmatize(w, pos=self.get_word_net_pos(p) if self.get_wordnet_pos(p) != '' \
+                                                      else self.lemmatizer.lemmatize(w) \
+                                                      for w, p in pos_tag(journal.body)]
         return journal
 
     def process_journals(self):
