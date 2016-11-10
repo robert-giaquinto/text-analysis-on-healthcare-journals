f505797d505e6710bb376dc2a1de2cab7a629c9d
robert-giaquinto <giaquinto.ra@gmail.com>
2016-11-10 10:33:04 -0600

    Updated, removed old scripts

--- scripts/run_dev_gensim_lda.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-DD=/home/srivbane/shared/caringbridge/data/dev/
-python -m src.topic_model.gensim_lda --journal_file ${DD}clean_journals/cleaned_journal100000_all.txt --data_dir ${DD}topic_model --keep_n 25000 --num_test 1000 --num_docs 91459 --num_topics 25 --chunksizes 256 --threshold 1.0 --n_workers 8 --evals_per_pass 2 --log --rebuild
