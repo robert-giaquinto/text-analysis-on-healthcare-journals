21ac23a6f23206d5a97301ec479dd159476c91d2
robert-giaquinto <giaquinto.ra@gmail.com>
2016-11-21 09:48:38 -0600

    Cleaned up code for parallel inference using LDA on new documents. Ran script for finding topic vectors for each docuemnt. Fixed typo in documents.py.

--- src/topic_model/gensim_lda.py
+++ src/topic_model/gensim_lda.py
@@ -527,48 +527,6 @@ def _handler(input, output):
         result = func(*args)
         output.put(result)
 
-
-def pool_imap_unordered(function, iterable, procs):
-    # Create queues for sending/receiving items from iterable.
-    sendq = Queue(procs)
-    recvq = Queue()
-
-    # Start worker processes.
-    for rpt in xrange(procs):
-        Process(target=_handler, args=(sendq, recvq)).start()
-
-    # Iterate iterable and communicate with worker processes.
-    send_len = 0
-    recv_len = 0
-    itr = iter(iterable)
-    try:
-        value = itr.next()
-        while True:
-            try:
-                sendq.put((function, value), True, 0.1)
-                send_len += 1
-                value = itr.next()
-            except QueueFull:
-                while True:
-                    try:
-                        result = recvq.get(False)
-                        recv_len += 1
-                        yield result
-                    except QueueEmpty:
-                        break
-    except StopIteration:
-        pass
-
-    # Collect all remaining results.
-    while recv_len < send_len:
-        result = recvq.get()
-        recv_len += 1
-        yield result
-
-    # Terminate worker processes.
-    for rpt in xrange(procs):
-        sendq.put(None)
-
         
 def _doc_topic_worker(bow_chunk, keys_chunk, output_file, lda):
     """
